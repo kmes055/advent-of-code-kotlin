4483eeb05fd8f13df476cfb98001bca30b8a7edd
marvin.kim <marvin.kim@kakaobank.com>
2022-12-13 18:54:39 +0900

    Make template

--- src/Day01.kt
+++ src/Day01.kt
@@ -19,10 +19,6 @@ fun main() {
             .sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-//    val testInput = readInput("Day01_test")
-//    check(part1(testInput) == 123)
-
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
