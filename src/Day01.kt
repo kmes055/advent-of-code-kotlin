e2465885a2366899b61c146dd7db461aea89a7bc
marvin.kim <marvin.kim@kakaobank.com>
2022-12-13 13:17:04 +0900

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,27 @@
 fun main() {
+    fun List<String>.sumSplit(predicate: (String) -> Boolean): List<Int> {
+        if (this.isEmpty()) return listOf()
+
+        val left = this.takeWhile(predicate).sumOf { it.toInt() }
+        val right = this.dropWhile(predicate).drop(1)
+        return listOf(left) + right.sumSplit(predicate)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumSplit { it.isNotBlank() }
+            .sortedDescending()[0]
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumSplit { it.isNotBlank() }
+            .sortedDescending()
+            .subList(0, 3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 123)
 
     val input = readInput("Day01")
     part1(input).println()
