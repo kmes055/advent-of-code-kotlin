859f63b155372ff9f40f45642e59c442f40bd184
marvin.kim <marvin.kim@kakaobank.com>
2022-12-27 21:18:19 +0900

    Update template2

--- src/Template.kt
+++ src/Template.kt
@@ -7,7 +7,18 @@ fun main() {
         return input.size
     }
 
-    val input = readInput("DayXX")
+    val day = 1
+    val dayString = String.format("%02d", day)
+
+
+    //
+    // -----------------------------
+    //
+    val testInput = readInput("Day${dayString}_test")
+//    checkEquals(part1(testInput), TODO())
+//    checkEquals(part2(testInput), TODO())
+
+    val input = readInput("Day$dayString")
     part1(input).println()
     part2(input).println()
 }
